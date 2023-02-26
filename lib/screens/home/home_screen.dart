daa7b6a6b28205f392aeb5fb14e44a636ac41357
AdoboPepper <jasper.calimbo@usjr.edu.ph>
2023-02-26 13:34:12 +0800

    Adds AppBar

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,10 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:food_app_ui/constants.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Column(
+          children: [
+            Text(
+              "Deliver to".toUpperCase(),
+              style: Theme.of(context)
+                  .textTheme
+                  .caption!
+                  .copyWith(color: kActiveColor),
+            ),
+            Text(
+              "San Francisco",
+              style: TextStyle(color: Colors.black),
+            ),
+          ],
+        ),
+        actions: [
+          TextButton(
+              onPressed: () {},
+              child: Text(
+                "Filter",
+                style: TextStyle(color: Colors.black),
+              ))
+        ],
+      ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            title: Column(
+              children: [
+                Text(
+                  "Deliver to".toUpperCase(),
+                  style: Theme.of(context)
+                      .textTheme
+                      .caption!
+                      .copyWith(color: kActiveColor),
+                ),
+                Text(
+                  "San Francisco",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ],
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Filter",
+                    style: TextStyle(color: Colors.black),
+                  ))
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
